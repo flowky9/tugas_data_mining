c577f34c18eba452a869b6e19a8373e8bc7ee1b7
nauvalshidqi <nauvals78@gmail.com>
2018-04-18 15:15:36 +0700

    first commit

---
+++ function/connection.php
@@ -0,0 +1,9 @@
+<?php 
+
+
+$connect = mysqli_connect("localhost","root","","datamining") OR die('gagal koneksi');
+
+$connect_clean = mysqli_connect("localhost","root","","datamining_clean") OR die('gagal koneksi');
+
+
+ ?>
